8ac48d0555db04e1f237d12760d21100caae4355
Mrunal <gaikar.mrunal@gmail.com>
2018-08-19 14:47:54 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+import math as m
+
+n=int(input())
+t=m.ceil(m.sqrt(n))
+
+res=n
+for x in range(2,t+1):
+    
+    if n%x==0:
+        while n%x==0:
+            n=n/x
+        res*=1-(1/x)
+          
+if n>1:
+    res*=1-(1/n)
+print(round(res))
